ea787b4f095d79fb041d94cd181ec27803fa3240
KB1101 <kornelgcc@gmail.com>
2016-06-15 15:33:13 +0200

    Update DesktopLauncher.java
    zmiany w try{}

--- desktop/src/com/mygdx/tanks/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/tanks/desktop/DesktopLauncher.java
@@ -12,16 +12,15 @@ public class DesktopLauncher {
 
 	public static void main (String[] arg) {
 		try {
-			// trzeba jeszcze zamykać socket ale  nie wiem gdzie , na razie
-		} catch (Exception ex){
-			System.out.println(ex);
-		} finally {
 			LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 			config.width = 800;
 			config.height= 800;
 			config.resizable = false;
 			config.backgroundFPS = 60;
 			new LwjglApplication(new TanksGame(magazine), config);
+			// trzeba jeszcze zamykać socket ale  nie wiem gdzie , na razie
+		} catch (Exception ex){
+			System.out.println(ex);
 		}
 
 	}
